27592af3355533e2f1d5f3c778c4d8349fdca5c1
KenWhite02 <whiteken2002@gmail.com>
2021-12-02 21:08:10 +0300

    Added fetch requests

---
+++ utils/requests.js
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-anonymous-default-export */
+const API_KEY = process.env.API_KEY;
+
+export default {
+  fetchTrending: {
+    title: 'Trending',
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: 'Top rated',
+    url: `/movie/top_rated?api_key=${API_KEY}&language=`,
+  },
+  fetchActionMovies: {
+    title: 'Action',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
+  fetchComedyMovies: {
+    title: 'Comedy',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  fetchHorrorMovies: {
+    title: 'Horror',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchRomanceMovies: {
+    title: 'Romance',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  fetchMystery: {
+    title: 'Mystery',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
+  fetchScifi: {
+    title: 'Sci-Fi',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  },
+  fetchWestern: {
+    title: 'Western',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  },
+  fetchAnimation: {
+    title: 'Animation',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  },
+  fetchTV: {
+    title: 'TV Shows',
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  },
+};
